8a61998150066d6faeb0efb4a2d74c6dd41b269d
dminikulin <dminikulin2003@gmail.com>
2025-09-29 12:47:18 +0300

    Add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs/promises');
+
+async function copy() {
+  const args = process.argv.slice(2);
+  const [oldPath, newPath] = args;
+
+  if (!oldPath || !newPath) {
+    console.error('Source and destination file paths are required');
+
+    return;
+  }
+
+  if (oldPath === newPath) {
+    return;
+  }
+
+  try {
+    await fs.copyFile(oldPath, newPath);
+  } catch (err) {
+    console.error('Copy error:', err.message);
+  }
+}
+
+if (require.main === module) {
+  copy();
+}
+
+module.exports = {
+  copy,
+};
